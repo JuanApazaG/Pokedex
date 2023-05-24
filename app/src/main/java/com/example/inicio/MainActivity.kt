4630e4f1c5ab47382f243df343d77488264090da
Juan Agustin Apaza <111205513+JuanApazaG@users.noreply.github.com>
2023-05-24 09:38:16 -0400

    primeros camvios

---
+++ app/src/main/java/com/example/inicio/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.inicio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val buttonAjustes = findViewById<Button>(R.id.buttonAjustes) as Button
+
+        val buttonAcercaDe = findViewById<Button>(R.id.buttonAcercaDe) as Button
+        val buttonSalir = findViewById<Button>(R.id.buttonSalir) as Button
+        val buttonListaPokemon = findViewById<Button>(R.id.buttonListaPokemon) as Button
+
+
+        buttonAjustes.setOnClickListener {
+            val intent = Intent (this@MainActivity,Ajustes::class.java)
+            startActivity(intent)
+        }
+        buttonAcercaDe.setOnClickListener {
+            val intent = Intent (this@MainActivity,AcercaDe::class.java)
+            startActivity(intent)
+        }
+
+        buttonSalir.setOnClickListener {
+            finish()
+        }
+        buttonListaPokemon.setOnClickListener {
+            val intent = Intent (this@MainActivity,ListaPokemon::class.java)
+            startActivity(intent)
+        }
+    }
+}
