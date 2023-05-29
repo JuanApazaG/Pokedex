d9b9263614e544dd9585a6c77535c16a2ced7c94
César L Vargas Lezano <cesarluisvargaslezano@gmail.com>
2023-05-28 21:22:39 -0400

    Añadi la pantalla 5 parar el pokemon seleccionado

--- app/src/main/java/com/example/inicio/pokemon_selec.kt
+++ app/src/main/java/com/example/inicio/pokemon_selec.kt
@@ -3,9 +3,9 @@ package com.example.inicio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Ajustes : AppCompatActivity() {
+class pokemon_selec : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ajustes)
+        setContentView(R.layout.activity_pokemon_selec)
     }
 }
