c474c407ad275299ed490f9023551c5b123df5f7
Luis Gustavo Vera Miranda <vera.miranda.luis.gustavo@gmail.com>
2023-05-30 19:24:09 -0400

    configuracion del inicio terminada

--- app/src/main/java/com/example/inicio/LoginActivity.kt
+++ app/src/main/java/com/example/inicio/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.example.inicio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val buttonIngresar = findViewById<Button>(R.id.butooningresar) as Button
+
+
+        buttonIngresar.setOnClickListener {
+            val intent = Intent (this@LoginActivity,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
